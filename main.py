76f14e9b84e8b918772b7cc2da8dc9e7fb3e3dff
Jakub Jarosiński <dzejkopjarosinski@gmail.com>
2022-05-10 19:48:41 +0200

    Function corrected, checking checker reaction to 0 as K

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def Newton(n,k):
 
     n_fact = math.factorial(n)
     k_fact = math.factorial(k)
-    nk_fact = math.factorial(n-k)
+    nk_fact = math.factorial((n-k))
 
-    newt_pattern = n_fact/k_fact*nk_fact
+    newt_pattern = n_fact/(k_fact*nk_fact)
     print(newt_pattern)
 
 
