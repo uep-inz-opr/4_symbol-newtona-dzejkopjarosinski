fe24476e8fd785ff5c1462c9806ba9cbc2f9f4e6
Jakub Jarosiński <dzejkopjarosinski@gmail.com>
2022-05-18 19:00:37 +0200

    hopefully last commit 2, split added to the input

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import math
 from math import factorial
 
-input = list(input())
+input = list(input().split())
 n = int(input[0])
 k = int(input[-1])
 
 
 
+
+
 def Newton(n,k):
+
 
     n_fact = math.factorial(n)
     k_fact = math.factorial(k)
     nk_fact = math.factorial((n-k))
 
     newt_pattern = int(n_fact/(k_fact*nk_fact))
+
     print(newt_pattern)
 
 
